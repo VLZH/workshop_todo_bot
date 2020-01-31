0c73c55fd7d7decc5929374e4a714704db5c1077
vlzh <vlzh@pm.me>
2020-01-31 12:22:09 +0600

    init

---
+++ todo_manager.py
@@ -0,0 +1,9 @@
+class TodosManager():
+    def __init__(self):
+        self.todos = list()
+
+    def addTodo(self, todo):
+        self.todos.append(todo)
+
+    def removeTodo(self, todo_id):
+        pass
